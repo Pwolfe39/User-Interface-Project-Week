9d3044325d68c96adb90da2c9d95295b640dfc7b
peter-wolfe <peterwolfe35@gmail.com>
2019-03-02 01:09:19 -0800

    Added navigation system design

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // JS goes here
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open');   
+    
+ };
+ 
+ const menu = document.querySelector('.menu');
+ const menuIcon = document.querySelector('.menu-icon');
+ const closeBtn = document.querySelector('#menu__close');
+ menuIcon.addEventListener('click', toggleMenu, false);
+ 
+ closeBtn.addEventListener('click', () => {
+   toggleMenu();
+ }, false);
+ 
