163502705cf0936e26b75b474ab6b8b420d1b9b0
peter-wolfe <peterwolfe35@gmail.com>
2019-03-05 04:32:48 -0800

    Added Services page mobile design

--- js/index.js
+++ js/index.js
@@ -1,55 +1,18 @@
 
 // // JS goes here
-// const toggleMenu = () => {
-//     menu.classList.toggle('menu--open');   
-    
-//  };
- 
-//  const menu = document.querySelector('.menu');
-//  const menuIcon = document.querySelector('.menu-icon');
-//  const closeBtn = document.querySelector('#menu__close');
-//  menuIcon.addEventListener('click', toggleMenu, false);
- 
-//  closeBtn.addEventListener('click', () => {
-//    toggleMenu();
-//  }, false);
-
-
-
-class TabLink {
-  constructor(element) {
-    this.element = element;
-    this.data = this.element.dataset.tab;
-    this.tabContent = document.querySelector(`.flex-construction[data-sec='${this.data}']`);
-    this.tabItem = new TabItem(this.tabContent);
-    this.element.addEventListener('click', () => {
-      this.select();
-    });
-  }
-  select() {
-    const links = document.querySelectorAll('.tab');
-    links.forEach((link) => {link.classList.remove('tabs-link-selected')});
-    this.element.classList.add('tabs-link-selected');
-    this.tabItem.select();
+const toggleMenu = () => {
+  menu.classList.toggle('menu--open');   
   
-  }
-  
-}
+};
 
-class TabItem {
-  constructor(element) {
-    this.element = element;
-  }
-  select() {
-    const items = document.querySelectorAll('.flex-construction');
-    items.forEach(item => item.classList.remove('tabs-item-selected'))
-    this.element.classList.add('tabs-item-selected');
-  }
-}
+const menu = document.querySelector('.menu');
+const menuIcon = document.querySelector('.menu-icon');
+const closeBtn = document.querySelector('#menu__close');
+menuIcon.addEventListener('click', toggleMenu, false);
 
-const links = document.querySelectorAll('.tab');
-links.forEach( element => new TabLink(element));
-
+closeBtn.addEventListener('click', () => {
+ toggleMenu();
+}, false);
 
 
 
---
+++ js/services.js
@@ -0,0 +1,37 @@
+
+class TabLink {
+    constructor(element) {
+      this.element = element;
+      this.data = this.element.dataset.tab;
+      this.tabContent = document.querySelector(`.flex-construction[data-sec='${this.data}']`);
+      this.tabItem = new TabItem(this.tabContent);
+      this.element.addEventListener('click', () => {
+        this.select();
+      });
+    }
+    select() {
+      const links = document.querySelectorAll('.tab');
+      links.forEach((link) => {link.classList.remove('tabs-link-selected')});
+      this.element.classList.add('tabs-link-selected');
+      this.tabItem.select();
+    
+    }
+    
+  }
+  
+  class TabItem {
+    constructor(element) {
+      this.element = element;
+    }
+    select() {
+      const items = document.querySelectorAll('.flex-construction');
+      items.forEach(item => item.classList.remove('tabs-item-selected'))
+      this.element.classList.add('tabs-item-selected');
+    }
+  }
+  
+  const links = document.querySelectorAll('.tab');
+  links.forEach( element => new TabLink(element));
+  
+  
+  
